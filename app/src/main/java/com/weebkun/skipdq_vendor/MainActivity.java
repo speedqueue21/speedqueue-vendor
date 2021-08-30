161ee9e6fcb74a38ff6801e36409d1b615c65b22
Weebkun <2002umpa@gmail.com>
2021-08-30 11:28:33 +0800

    ok

---
+++ app/src/main/java/com/weebkun/skipdq_vendor/MainActivity.java
@@ -0,0 +1,16 @@
+package com.weebkun.skipdq_vendor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.weebkun.skipdq_net.HttpClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
